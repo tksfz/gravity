abf6bb229a7223c86ff21f3d7a05408eb23774d8
thom <tom@tksfz.org>
2016-10-11 22:29:13 -0700

    Move Label stuff to its own file

--- app/src/main/scala/gravity/ui/View.scala
+++ app/src/main/scala/gravity/ui/View.scala
@@ -8,51 +8,6 @@ import shapeless.labelled._
 import shapeless.ops.hlist.{Mapper, ToTraversable, ZipConst}
 import shapeless.tag.@@
 
-/**
-  * For a field, `T` is of the form `FieldType[K, V] @@ C == V with KeyTag[K, V] with Tagged[C]` where C is
-  * the case class from which a record is derived containing field K with value V.  This allows us to define
-  * low-priority default implicits for T's based on K, V, or C alone or in some combination GIVEN THAT
-  * we have contravariant View[-T] http://stackoverflow.com/questions/6682824/how-can-i-combine-the-typeclass-pattern-with-subtyping
-  *
-  * "Label" is too broad here.  Really, these are always labels of a noun Thing.  E.g. we should add plural label here
-  * @tparam T
-  */
-trait Label[T] {
-  def label: String
-}
-
-object Label {
-  // Note that V is unconstrained
-  implicit def fromLabelsData[T, K, V, R <: HList]
-  (implicit
-    data: Labels[T, R],
-    kInR: ops.record.Selector.Aux[R, K, String]) = new Label[FieldType[K, V] @@ T] {
-    def label = kInR.apply(data.labels)
-  }
-}
-
-// Or rename to MetaMap or MetadataMap
-abstract class Metadata[C, M <: HList](val map: M)
-
-// SelectMany might be better here
-// to traversable String
-// TODO: how do we add the label for T itself here?
-// Instead of LabelledGeneric we might want to check labels against a set including labelledgeneric plus methods
-case class Labels[T, R <: HList](labels: R) extends Metadata[T, R](labels)
-
-object Labels {
-  class Curried[T] extends RecordArgs {
-    def applyRecord[R <: HList](labels: R) = {
-      Labels[T, R](labels)
-    }
-
-    //def apply[R <: HList](labels: R) = applyRecord(labels)
-  }
-  def apply[T] = new Curried[T]
-}
-
-
-
 // come up with a better name than Label
 // FieldHeader or FieldLabel? header is fine actually.  it doesn't have to be just a field header
 // e.g. it could be a tab header
