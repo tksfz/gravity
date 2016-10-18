9f289bb626433ec8bae7e021fa1dcd1a423a494f
thom <tom@tksfz.org>
2016-10-18 07:05:19 -0700

    Experiment: Referenceable

--- app/src/main/scala/gravity/models.scala
+++ app/src/main/scala/gravity/models.scala
@@ -1,6 +1,20 @@
 package gravity
 
+import shapeless.{HList, Witness}
+import shapeless.ops.record.Selector
+
 object models {
+
+  /*
+  trait Referenceable[S <: Symbol] {
+    def idField: S
+  } */
+  trait Referenceable[T, S <: Symbol] {
+    type L <: HList
+    def ev: Witness.Aux[S]
+    def ev2: Selector.Aux[L, S, ObjectId]
+    def idField: S
+  }
 
   trait Reference[T]
 
@@ -12,6 +26,7 @@ object models {
     def map[B](f: T => B): One[B] = ???
   }
   case class OneId[T](id: ObjectId) extends One[T]
+  // resolved should always have an id too
   //case class ResolvedOne[T, R](r: R) extends One[T]
 
   case class Phone(phone: String)
