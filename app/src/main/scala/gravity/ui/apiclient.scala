b3a581e74fe2fc0178460a76b358484795f47d5a
thom <tom@tksfz.org>
2016-11-18 15:27:23 -0800

    Comment

--- app/src/main/scala/gravity/ui/apiclient.scala
+++ app/src/main/scala/gravity/ui/apiclient.scala
@@ -3,6 +3,8 @@ package gravity.ui
 import scala.concurrent.Future
 
 object apiclient {
+
+  // TODO: replace this with a type Query[Id, Option[T]], or with some Auto-wire thing
   trait Get[T] {
     def get(id: Int): Future[Option[T]]
   }
