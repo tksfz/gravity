3d86df60d90540d80dc1bee1f4d8826e9112b3cc
thom <tom@tksfz.org>
2016-11-18 16:40:51 -0800

    Add page-components and routes for displaying the results of queries

--- app/src/main/scala/gravity/ui/ClassRoutes.scala
+++ app/src/main/scala/gravity/ui/ClassRoutes.scala
@@ -3,11 +3,12 @@ package gravity.ui
 import apiclient.Get
 import chandu0101.scalajs.react.components.WithAsyncScript
 import chandu0101.scalajs.react.components.materialui.{Mui, MuiAppBar, MuiIconButton, MuiMuiThemeProvider, MuiSvgIcon}
-import japgolly.scalajs.react.{Callback, ReactComponentB, ReactElement, ReactNode, TopNode}
-import japgolly.scalajs.react.extra.router.StaticDsl.Rule
-import japgolly.scalajs.react.extra.router.{RouterConfigDsl, RouterCtl}
+import japgolly.scalajs.react.{Callback, ReactComponentB, ReactComponentC, ReactElement, ReactNode, TopNode}
+import japgolly.scalajs.react.extra.router.StaticDsl.{Route, RouteB, Rule}
+import japgolly.scalajs.react.extra.router.{Path, RouterConfigDsl, RouterCtl}
 import japgolly.scalajs.react.vdom.prefix_<^._
 
+import scala.concurrent.Future
 import scala.reflect.ClassTag
 import scala.scalajs.js
 
@@ -28,6 +29,8 @@ trait ClassRoutes[T] {
 
 object ClassRoutes {
 
+
+  /** Define routes with this using `implicit val fooRoutes = ClassRoutes[Foo]( ...some routes ...)` */
   def apply[T](someRoutes: Rule[AnyPage]) = new ClassRoutes[T] {
     def routes = someRoutes
   }
@@ -40,6 +43,7 @@ object ClassRoutes {
     */
   case class ViewPage[T : ClassTag](id: Int) extends AnyPage
   case class EditPage[T : ClassTag](id: Int) extends AnyPage
+  case class ListPage[T : ClassTag]() extends AnyPage
 
   import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
   import japgolly.scalajs.react.vdom.Implicits._
@@ -63,8 +67,8 @@ object ClassRoutes {
     ct: ClassTag[T],
     get: Get[T],
     v: View[T]) = RouterConfigDsl[AnyPage].buildRule { dsl =>
-
     import dsl._
+
     val DetailPageComponent = singleRowPageComponent[T](v.view(_, _))
 
     dynamicRouteCT(("#" / ct.runtimeClass.getSimpleName / int).caseClass[ViewPage[T]]) ~>
@@ -72,7 +76,8 @@ object ClassRoutes {
         val editLink = { () =>
           router.link(EditPage[T](detailPage.id))(MuiIconButton()(Mui.SvgIcons.ImageEdit()()))
         }
-        DetailPageComponent(SingleRowPageProps(router, detailPage.id, iconElementRight = editLink))
+        val mainProps = MainLayoutProps(router, iconElementRight = editLink)
+        DetailPageComponent((detailPage.id, mainProps))
       }
   }
 
@@ -81,22 +86,20 @@ object ClassRoutes {
     ct: ClassTag[T],
     get: Get[T],
     e: Edit[T]) = RouterConfigDsl[AnyPage].buildRule { dsl =>
-
     import dsl._
+
     val EditPageComponent = singleRowPageComponent[T]({ case (router, t) => e.element(e.toModel(t)) })
+
     dynamicRouteCT(("#" / ct.runtimeClass.getSimpleName / int / "edit").caseClass[EditPage[T]]) ~>
       dynRenderR { (editPage, router) =>
         // TODO: only show backlink if there is a back in the history
         val backLink = { () =>
           <.a(^.href := "javascript:history.back()")(MuiIconButton()(Mui.SvgIcons.NavigationArrowBack()()))
         }
-        EditPageComponent(SingleRowPageProps(router, editPage.id, iconElementLeft = backLink))
+        val mainProps = MainLayoutProps(router, backLink)
+        EditPageComponent((editPage.id, mainProps))
       }
   }
-
-  case class SingleRowPageProps(router: RouterCtl[AnyPage], id: Int,
-    iconElementLeft: () => js.UndefOr[ReactElement] = { () => js.undefined },
-    iconElementRight: () => js.UndefOr[ReactElement] = { () => js.undefined })
 
   /**
     * ReactComponent for a Page that accepts an Id and uses a Get instance
@@ -104,20 +107,83 @@ object ClassRoutes {
     */
   def singleRowPageComponent[T](fn: (RouterCtl[AnyPage], T) => ReactNode)
   (implicit ct: ClassTag[T], get: Get[T]) =
-    ReactComponentB[SingleRowPageProps]("detailpage")
+    ReactComponentB[(Int, MainLayoutProps)]("detailpage")
       .initialState(Option.empty[T])
       .render(P =>
-        MainLayout(P.props.iconElementLeft(), P.props.iconElementRight()) {
+        MainLayout(P.props._2,
           P.state map { t =>
-            fn(P.props.router, t)
+            fn(P.props._2.router, t)
           } getOrElse {
-            s"${ct.runtimeClass.getSimpleName} ${P.props.id} not found".asInstanceOf[ReactNode]
+            s"${ct.runtimeClass.getSimpleName} ${P.props._1} not found".asInstanceOf[ReactNode]
           }
-        }
+        )
       )
       .componentDidMount(P => Callback.future {
-        get.get(P.props.id).map(P.setState(_))
+        get.get(P.props._1).map(P.setState(_))
       })
       .build
+
+  /**
+    * Page-level component for viewing a query
+    */
+  def queryViewPageComponent[T](initial: T, query: => Future[T])
+    (implicit v: View[T]): ReactComponentC.ReqProps[MainLayoutProps, T, Unit, TopNode] = queryViewPageComponent(initial, query, v.view)
+
+  /**
+    * Page-level component for viewing a query
+    * TODO: abstract this so that it can also be used with Edit[T] or arbitrary F[T]
+    */
+  def queryViewPageComponent[T](initial: T, query: => Future[T],
+    renderFn: (RouterCtl[AnyPage], T) => ReactNode): ReactComponentC.ReqProps[MainLayoutProps, T, Unit, TopNode] =
+  ReactComponentB[MainLayoutProps]("viewpage")
+    .initialState(initial)
+    .render(P => MainLayout(P.props, renderFn(P.props.router, P.state)))
+    .componentDidMount(P => Callback.future {
+      query.map(P.setState(_))
+    })
+    .build
+
+  def classListPageRoute[T](getData: => Future[Seq[T]])
+  (implicit
+    ct: ClassTag[T],
+    v: View[Seq[T]]) = {
+    // these implicits copied from Dsl.scala should be put somewhere more accessible
+    implicit def _auto_routeB_from_str(l: String) = RouteB.literal(l)
+    implicit def _auto_route_from_routeB[A, R <% RouteB[A]](r: R) = r.route
+
+    staticListPageRoute("#" / ct.runtimeClass.getSimpleName, ListPage[T](), getData)
+  }
+
+  def staticListPageRoute[P <: AnyPage, T](route: Route[Unit], page: P, getData: => Future[Seq[T]])
+    (implicit v: View[Seq[T]]) = {
+    staticQueryPageRoute(route, page, Nil, getData)
+  }
+
+  def staticQueryPageRoute[P <: AnyPage, T](route: Route[Unit], page: P, initial: T, getData: => Future[T])
+    (implicit
+      v: View[T]): Rule[AnyPage] = RouterConfigDsl[AnyPage].buildRule { dsl =>
+    import dsl._
+
+    // break this up into helper functions and lift up to caller
+    staticRoute(route, page) ~>
+      renderR {
+        staticQueryPageRender(initial, getData)
+      }
+    //dynamicRouteCT(("#" / ct.runtimeClass.getSimpleName).caseClass[ListPage[T]]) ~>
+    //  dynRenderR { dynListPageRender({ _ => getData }) }
+  }
+
+  def staticQueryPageRender[T](initial: T, getData: => Future[T])
+    (implicit
+      v: View[T]): RouterCtl[AnyPage] => ReactElement = { router =>
+    dynQueryPageRender(initial, { (_: Unit) => getData }).apply((), router)
+  }
+
+  def dynQueryPageRender[P, T](initial: T, getData: P => Future[T])
+    (implicit
+      v: View[T]): (P, RouterCtl[AnyPage]) => ReactElement = { (p: P, router) =>
+    val ListPageComponent = queryViewPageComponent[T](initial, getData(p))
+    ListPageComponent(MainLayoutProps(router))
+  }
 
 }
--- app/src/main/scala/gravity/ui/package.scala
+++ app/src/main/scala/gravity/ui/package.scala
@@ -2,6 +2,7 @@ package gravity
 
 import chandu0101.scalajs.react.components.WithAsyncScript
 import chandu0101.scalajs.react.components.materialui.{MuiAppBar, MuiMuiThemeProvider}
+import japgolly.scalajs.react.extra.router.RouterCtl
 import japgolly.scalajs.react.{ReactComponentB, ReactElement}
 import japgolly.scalajs.react.vdom.prefix_<^._
 
@@ -26,31 +27,34 @@ package object ui {
 
   import chandu0101.scalajs.react.components.Implicits._
 
+  case class MainLayoutProps(
+    router: RouterCtl[AnyPage],
+    iconElementLeft: () => js.UndefOr[ReactElement] = { () => js.undefined },
+    iconElementRight: () => js.UndefOr[ReactElement] = { () => js.undefined })
+
   /**
     * the Prop has to be lazy. otherwise if you reference Mui elements you get undefined errors
     * I think because the Mui global isn't defined until you get inside this component
     * TODO: consider moving to a super-trait Layouts.scala or some other file
     */
-  def MainLayout(
-    iconElementLeft: => js.UndefOr[ReactElement] = js.undefined,
-    iconElementRight: => js.UndefOr[ReactElement] = js.undefined) =
-  ReactComponentB[Unit]("layout")
-    .render(P =>
-      WithAsyncScript("assets/material_ui-bundle.js") {
-        MuiMuiThemeProvider()(
-          <.div(
-            MuiAppBar(
-              title = "Title",
-              showMenuIconButton = true,
-              iconElementLeft = iconElementLeft,
-              iconElementRight = iconElementRight
-            )(),
-            P.propsChildren
+  val MainLayout =
+    ReactComponentB[MainLayoutProps]("layout")
+      .render(P =>
+        WithAsyncScript("assets/material_ui-bundle.js") {
+          MuiMuiThemeProvider()(
+            <.div(
+              MuiAppBar(
+                title = "Title",
+                showMenuIconButton = true,
+                iconElementLeft = P.props.iconElementLeft(),
+                iconElementRight = P.props.iconElementRight()
+              )(),
+              P.propsChildren
+            )
           )
-        )
-      }
-    )
-    .build
+        }
+      )
+      .build
 
 
 }
