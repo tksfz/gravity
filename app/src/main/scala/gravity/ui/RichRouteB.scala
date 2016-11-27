74598e313fdf97c6ef69ad740682fc44e961045d
thom <tom@tksfz.org>
2016-11-27 09:34:49 -0800

    Fix compilation

--- app/src/main/scala/gravity/ui/RichRouteB.scala
+++ app/src/main/scala/gravity/ui/RichRouteB.scala
@@ -31,49 +31,4 @@ object generic {
       }
   }
 
-
-  implicit class RichRouteB[A <: Product](val ra: RouteB[A]) {
-
-    class Curried[B] {
-      def apply[AL <: HList, BL <: HList]
-      (implicit
-        ag: Generic.Aux[A, AL],
-        bg: Generic.Aux[B, BL],
-        eq: AL =:= BL,
-        eq2: BL =:= AL
-      ): RouteB[B] = {
-        new RouteB[B](
-          regex = ra.regex,
-          matchGroups = ra.matchGroups,
-          parse = ra.parse(_) map { a =>
-            val al = ag.to(a)
-            val bl: BL = al
-            bg.from(bl)
-          },
-          build = { b =>
-            val bl = bg.to(b)
-            val al: AL = bl
-            val a = ag.from(al)
-            ra.build(a)
-          }
-        )
-      }
-    }
-
-    def caseClassShapeless[B] = new Curried[B]
-
-    def caseClassShapeless2[B]
-    (implicit tg: TupleGeneric.Aux[A, B]): RouteB[B] = {
-      new RouteB[B](
-        regex = ra.regex,
-        matchGroups = ra.matchGroups,
-        parse = ra.parse(_) map { a =>
-          tg.to(a)
-        },
-        build = { b =>
-          ra.build(tg.from(b))
-        }
-      )
-    }
-  }
 }
--- app/src/main/scala/gravity/ui/Routable.scala
+++ app/src/main/scala/gravity/ui/Routable.scala
@@ -3,30 +3,44 @@ package gravity.ui
 import generic._
 import shapeless.{$up => _, _}
 
-trait Routable[T, P] {
+/**
+  * Type class supporting injection of app page types into libraries that use those pages (either
+  * generating page contents or linking to them)
+  *
+  * @tparam P page type
+  * @tparam T route arguments from which the page can be instantiated or destructured to
+  */
+trait Routable[P, T] {
   def apply(t: T): P
   def unapply(p: P): T
 }
 
 object Routable {
-  // FIXME: this is backwards
-  implicit def routableTupleCaseClass[T <: Product, P]
-  (implicit tg: TupleGeneric.Aux[T, P]) = new Routable[T, P] {
-    def apply(t: T) = tg.to(t)
-    def unapply(p: P) = tg.from(p)
+
+  /**
+    * @tparam C a case class
+    * @tparam T a tuple type
+    */
+  implicit def routableTupleCaseClass[C <: Product, T]
+  (implicit tg: TupleGeneric.Aux[C, T]) = new Routable[C, T] {
+    def apply(t: T) = tg.from(t)
+    def unapply(p: C) = tg.to(p)
   }
 
-  // tuple1
-  implicit def routableTuple1CaseClass[A, P <: Product]
-  (implicit tg: TupleGeneric.Aux[P, Tuple1[A]]) = new Routable[A, P] {
+  /**
+    * @tparam C a case class
+    * @tparam A a non-product type (not a tuple)
+    */
+  implicit def routableTuple1CaseClass[C <: Product, A]
+  (implicit tg: TupleGeneric.Aux[C, Tuple1[A]]) = new Routable[C, A] {
     def apply(a: A) = tg.from(Tuple1(a))
-    def unapply(p: P) = tg.to(p)._1
+    def unapply(p: C) = tg.to(p)._1
   }
 
   // TODO: unit
 
   // custom routable
-  def routable[T, P](a: T => P, u: P => T) = new Routable[T, P] {
+  def routable[P, T](a: T => P, u: P => T) = new Routable[P, T] {
     def apply(t: T) = a(t)
     def unapply(p: P) = u(p)
   }
