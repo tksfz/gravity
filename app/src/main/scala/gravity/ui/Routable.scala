8db4132d5c8ff7d518e39c70cd033d3232342fe5
thom <tom@tksfz.org>
2016-11-29 06:53:35 -0800

    Move representation type parameter in Routable/Linkable/Pathable to type member

--- app/src/main/scala/gravity/ui/Routable.scala
+++ app/src/main/scala/gravity/ui/Routable.scala
@@ -1,8 +1,6 @@
 package gravity.ui
 
 import generic._
-import shapeless.HList
-import shapeless.ops.hlist.{LiftAll, Selector, SubtypeUnifier}
 
 /**
   * Type class supporting linking to a page type P that accepts argument T
@@ -10,40 +8,30 @@ import shapeless.ops.hlist.{LiftAll, Selector, SubtypeUnifier}
   * @tparam P page type
   * @tparam T route arguments from which the page can be instantiated
   */
-trait Linkable[+P, -T] {
-  def apply(t: T): P
+trait Linkable[+P] {
+  type Repr
+  def apply(t: Repr): P
 }
 
 // TODO: static pages
 object Linkable {
 
-  type Lamb[S] = { type Da[Q] = Linkable[Q, S] }
-
-  // liftall implicit
-  case class Links[L <: HList]()
+  type Aux[Page, T] = Linkable[Page] { type Repr = T }
 
   // routable => linkable
-  implicit def linkableFromRoutable[C <: Product, T]
-  (implicit routable: Routable[C, T]) = new Linkable[C, T] {
-    override def apply(t: T): C = routable.apply(t)
-  }
-
-  implicit def linkableFromLinks2[Sup, T, L <: HList, I <: HList, J <: HList]
-  (implicit
-    links: Links[L],
-    liftAll: LiftAll.Aux[Lamb[T]#Da, L, I], // this is wrong: we want LiftSome
-    unify: SubtypeUnifier.Aux[I, Linkable[Sup, T], J],
-    select: Selector[J, Linkable[Sup, T]]
-  ) = new Linkable[Sup, T] {
-    override def apply(t: T): Sup = select(unify(liftAll.instances)).apply(t)
+  implicit def linkableFromRoutable[C <: Product]
+  (implicit routable: Routable[C]) = new Linkable[C] {
+    type Repr = routable.Repr
+    override def apply(t: Repr): C = routable.apply(t)
   }
 
   implicit def some[P, T]
-  (implicit linkable: Linkable[P, T]): Option[Linkable[P, T]] = Some(linkable)
+  (implicit linkable: Linkable[P]): Option[Linkable[P]] = Some(linkable)
 }
 
-trait Pathable[-P, +T] {
-  def unapply(p: P): T
+trait Pathable[-P] {
+  type Repr
+  def unapply(p: P): Repr
 }
 
 /**
@@ -53,24 +41,22 @@ trait Pathable[-P, +T] {
   * @tparam P page type
   * @tparam T route arguments from which the page can be instantiated or destructured to
   */
-trait Routable[P, T] extends Linkable[P, T] with Pathable[P, T]
+trait Routable[P] extends Linkable[P] with Pathable[P]
 
 object Routable {
 
-  class Curried[P] {
-    def apply[T]
-    (implicit routable: Routable[P, T]) = routable
-  }
+  type Aux[Page, T] = Routable[Page] { type Repr = T }
 
-  def apply[P] = new Curried[P]
+  def apply[P : Routable] = implicitly[Routable[P]]
 
   /**
     * @tparam C a case class
     * @tparam T a tuple type
     */
-  implicit def routableTupleCaseClass[C <: Product, T <: Product]
-  (implicit tg: TupleGeneric.Aux[C, T]) = new Routable[C, T] {
-    def apply(t: T) = tg.from(t)
+  implicit def routableTupleCaseClass[C <: Product]
+  (implicit tg: TupleGeneric[C]) = new Routable[C] {
+    type Repr = tg.Repr
+    def apply(t: Repr) = tg.from(t)
     def unapply(p: C) = tg.to(p)
   }
 
@@ -79,15 +65,17 @@ object Routable {
     * @tparam A a non-product type (not a tuple)
     */
   implicit def routableTuple1CaseClass[C <: Product, A]
-  (implicit tg: TupleGeneric.Aux[C, Tuple1[A]]) = new Routable[C, A] {
-    def apply(a: A) = tg.from(Tuple1(a))
+  (implicit tg: TupleGeneric.Aux[C, Tuple1[A]]) = new Routable[C] {
+    type Repr = A
+    def apply(a: Repr) = tg.from(Tuple1(a))
     def unapply(p: C) = tg.to(p)._1
   }
 
   // TODO: unit
 
   // custom routable
-  def routable[P, T](a: T => P, u: P => T) = new Routable[P, T] {
+  def routable[P, T](a: T => P, u: P => T) = new Routable[P] {
+    type Repr = T
     def apply(t: T) = a(t)
     def unapply(p: P) = u(p)
   }
